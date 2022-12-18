8cc168c31ee247e3b41287e177fc9c06f9656d9e
PurosovMV <mihail.purosov@hotmail.com>
2022-12-18 22:45:39 +0300

    Решена задача №2

---
+++ Задание 2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Программа находит максимальное из трёх чисел");
+Console.WriteLine("Введи через пробел три числа ");
+string[] i = Console.ReadLine().Split(' ');
+int a = Convert.ToInt32(i[0]);
+int b = Convert.ToInt32(i[1]);
+int c = Convert.ToInt32(i[2]);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write($"{a}, {b}, {c} -> {max}");
